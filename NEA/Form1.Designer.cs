32b30b15252559a20a6e0ac0539f982fefa042b1
PiccPig <43648001+PiccPig@users.noreply.github.com>
2021-10-21 09:15:07 +0100

    Add files via upload

---
+++ NEA/Form1.Designer.cs
@@ -0,0 +1,159 @@
+﻿
+using System;
+
+namespace NEA
+{
+    partial class MatchMaker
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SizeLabel = new System.Windows.Forms.Label();
+            this.LengthLabel = new System.Windows.Forms.Label();
+            this.WidthLabel = new System.Windows.Forms.Label();
+            this.LengthUpDown = new System.Windows.Forms.NumericUpDown();
+            this.WidthUpDown = new System.Windows.Forms.NumericUpDown();
+            this.SizeSubmitButton = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.LengthUpDown)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.WidthUpDown)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // SizeLabel
+            // 
+            this.SizeLabel.AutoSize = true;
+            this.SizeLabel.Location = new System.Drawing.Point(12, 29);
+            this.SizeLabel.Name = "SizeLabel";
+            this.SizeLabel.Size = new System.Drawing.Size(30, 15);
+            this.SizeLabel.TabIndex = 0;
+            this.SizeLabel.Text = "Size:";
+            // 
+            // LengthLabel
+            // 
+            this.LengthLabel.AutoSize = true;
+            this.LengthLabel.Location = new System.Drawing.Point(54, 3);
+            this.LengthLabel.Name = "LengthLabel";
+            this.LengthLabel.Size = new System.Drawing.Size(47, 15);
+            this.LengthLabel.TabIndex = 1;
+            this.LengthLabel.Text = "Length:";
+            // 
+            // WidthLabel
+            // 
+            this.WidthLabel.AutoSize = true;
+            this.WidthLabel.Location = new System.Drawing.Point(115, 2);
+            this.WidthLabel.Name = "WidthLabel";
+            this.WidthLabel.Size = new System.Drawing.Size(42, 15);
+            this.WidthLabel.TabIndex = 2;
+            this.WidthLabel.Text = "Width:";
+            // 
+            // LengthUpDown
+            // 
+            this.LengthUpDown.Location = new System.Drawing.Point(54, 27);
+            this.LengthUpDown.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.LengthUpDown.Name = "LengthUpDown";
+            this.LengthUpDown.ReadOnly = true;
+            this.LengthUpDown.Size = new System.Drawing.Size(52, 23);
+            this.LengthUpDown.TabIndex = 3;
+            this.LengthUpDown.Value = new decimal(new int[] {
+            20,
+            0,
+            0,
+            0});
+            // 
+            // WidthUpDown
+            // 
+            this.WidthUpDown.Increment = new decimal(new int[] {
+            2,
+            0,
+            0,
+            0});
+            this.WidthUpDown.Location = new System.Drawing.Point(115, 27);
+            this.WidthUpDown.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.WidthUpDown.Name = "WidthUpDown";
+            this.WidthUpDown.ReadOnly = true;
+            this.WidthUpDown.Size = new System.Drawing.Size(52, 23);
+            this.WidthUpDown.TabIndex = 4;
+            this.WidthUpDown.Value = new decimal(new int[] {
+            9,
+            0,
+            0,
+            0});
+            this.WidthUpDown.ValueChanged += new System.EventHandler(this.WidthUpDown_ValueChanged);
+            // 
+            // SizeSubmitButton
+            // 
+            this.SizeSubmitButton.Location = new System.Drawing.Point(197, 25);
+            this.SizeSubmitButton.Name = "SizeSubmitButton";
+            this.SizeSubmitButton.Size = new System.Drawing.Size(75, 23);
+            this.SizeSubmitButton.TabIndex = 5;
+            this.SizeSubmitButton.Text = "Submit";
+            this.SizeSubmitButton.UseVisualStyleBackColor = true;
+            this.SizeSubmitButton.Click += new System.EventHandler(this.SizeSubmitButton_Click);
+            // 
+            // MatchMaker
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(484, 661);
+            this.Controls.Add(this.SizeSubmitButton);
+            this.Controls.Add(this.WidthUpDown);
+            this.Controls.Add(this.LengthUpDown);
+            this.Controls.Add(this.WidthLabel);
+            this.Controls.Add(this.LengthLabel);
+            this.Controls.Add(this.SizeLabel);
+            this.MinimumSize = new System.Drawing.Size(500, 500);
+            this.Name = "MatchMaker";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.LengthUpDown)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.WidthUpDown)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        private void WidthUpDown_ValueChanged(object sender, EventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label SizeLabel;
+        private System.Windows.Forms.Label LengthLabel;
+        private System.Windows.Forms.Label WidthLabel;
+        private System.Windows.Forms.NumericUpDown LengthUpDown;
+        private System.Windows.Forms.NumericUpDown WidthUpDown;
+        private System.Windows.Forms.Button SizeSubmitButton;
+    }
+}
+
---
+++ NEA/Form1.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace NEA
+{
+    public partial class MatchMaker : Form
+    {
+        private int gridLength = 20;
+        private int gridWidth = 9;
+        private NoteButton[,] notes;
+        private int gridSizeHorizontal = 270;
+        private int gridSizeVertical = 600;
+
+        public MatchMaker()
+        {
+            InitializeComponent();
+            MatchMaker_Load();
+        }
+
+        public void MatchMaker_Load()
+        {
+            CreateGrid(gridLength, gridWidth, gridSizeVertical, gridSizeHorizontal,50,50);
+        }
+
+        public void CreateGrid(int gridLength, int gridWidth, int gridSizeVertical, int gridSizeHorizontal, int topBuffer, int sideBuffer)
+        {
+            int buttonWidth = gridSizeHorizontal / gridWidth;
+            int buttonHeight = gridSizeVertical / gridLength;
+            notes = new NoteButton[gridWidth, gridLength];
+            for(int j = 0 ; j < gridLength ; j++ )
+            {
+                for (int i = 0; i < gridWidth; i++)
+                {
+                    notes[i, j] = new NoteButton()
+                    {
+                        Location = new Point((i * buttonWidth) + sideBuffer, (j * buttonHeight) + topBuffer),
+                        Size = new Size(buttonWidth, buttonHeight),
+                        BackColor = Color.Gray,
+                        ForeColor = Color.DarkGray,
+                        Colour = 0
+                    };
+                    notes[i, j].Click += new EventHandler(NoteButton_Click);
+                    Controls.Add(notes[i, j]);
+                    
+                }
+            }
+        }
+
+        private void NoteButton_Click(object sender, EventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void SizeSubmitButton_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
